0d9b0a8e4e2188e54a80fb77c863cd73e1e7f50a
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-06 20:01:57 +0100

    Add template content

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -2,6 +2,17 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'demo-app',
-  template: '<hello-world></hello-world>'
+  template: `
+     <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+                  <h2>Matches requested by round</h2>
+            </div>
+            <div class="panel-body">
+                <zssport-round [roundId]="2016170208"></zssport-round>
+            </div>
+        </div>
+    </div>
+  `
 })
 export class Demo {}
